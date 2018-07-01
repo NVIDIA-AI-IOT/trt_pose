11622b5f54932abf878fc9381eb4680d631eaa79
John Welsh <jwelsh@nvidia.com>
2018-07-01 23:54:20 +0000

    matrix_solve.cu

--- src/matrix_solve.h
+++ src/matrix_solve.h
@@ -3,35 +3,11 @@
 #include "cusolverDn.h"
 #include "matrix.h"
 
-int matrix_solve_c_workspace_size(
-    cusolverDnHandle_t handle,
-    float *a_data, matrix_t *a_mat)
-{
-  int workspace_size;
-  cusolverDnSpotrf_bufferSize(handle, CUBLAS_FILL_MODE_UPPER,
-      a_mat->rows, a_data, a_mat->rows, &workspace_size); 
-  return workspace_size;
-};
+int matrix_solve_c_workspace_size( cusolverDnHandle_t handle, float *a_data, matrix_t *a_mat);
+;
 
 // must populate b_mat with identity matrix
 // matrix must be symmetric positive-definite
-int matrix_solve_c(
-    cusolverDnHandle_t handle,
-    float *a_data, matrix_t *a_mat,
+int matrix_solve_c( cusolverDnHandle_t handle, float *a_data, matrix_t *a_mat,
     float *b_data, matrix_t *b_mat, 
-    float *workspace, int workspace_size)
-{
-  int *info;
-  cudaMalloc(&info, sizeof(int));
-
-  // cholesky factorization
-  cusolverDnSpotrf(handle, CUBLAS_FILL_MODE_UPPER, a_mat->rows, a_data,
-    a_mat->rows, workspace, workspace_size, info); 
-
-  // linear solve
-  cusolverDnSpotrs(handle, CUBLAS_FILL_MODE_UPPER, a_mat->rows, b_mat->cols,
-      a_data, a_mat->rows, b_data, b_mat->rows, info);
-
-  cudaFree(info);
-  return 0;
-}
+    float *workspace, int workspace_size);
