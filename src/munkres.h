9c32473961f2cdcb1f805da7770fb89f52d4f873
John Welsh <jwelsh@nvidia.com>
2018-07-06 21:27:09 +0000

    munkres step 3

--- src/munkres.h
+++ src/munkres.h
@@ -59,7 +59,7 @@ void munkres_step_1(float *a, bool *s, int n, int m)
   }
 }
 
-// returns true if finished
+// returns true if finished, c1 should be initialized 0
 bool munkres_step_2(bool *s, bool *c1, int n, int m)
 {
   int k = n < m ? n : m;
@@ -78,4 +78,57 @@ bool munkres_step_2(bool *s, bool *c1, int n, int m)
     }
   }
   return count >= k;
+}
+
+bool munkres_step_3_all_covered(float *a, bool *c0, bool *c1, int n, int m)
+{
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      if (a[IDX_2D(i, j, m)] == 0 && !c0[i] && !c1[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// p0, p1 will contain the row, col of the prime (applicable if return is true)
+bool munkres_step_3_prime(float *a, bool *c0, bool *c1, bool *s, bool *p, int *p0, int *p1, int n, int m)
+{
+  for (int i = 0; i < n; i++) {
+    if (c0[i]) {
+      continue;
+    }
+    for (int j = 0; j < m; j++) {
+      if (c1[j]) {
+        continue;
+      }
+      if (a[IDX_2D(i, j, m)] == 0) {
+        p[IDX_2D(i, j, m)] = true;
+        *p0 = i;
+        *p1 = j;
+        // cover row and uncover col of starred if exists
+        for (int k = 0; k < m; k++) {
+          if (s[IDX_2D(i, k, m)]) {
+           c0[i] = true;
+           c1[k] = false; 
+           return false; // remain in procedure 3
+          }
+        }
+        return true; // go to procedure 4
+      }
+    }
+  }
+  return false; // remain in procedure 3 (really, go to 5)
+}
+
+bool munkres_step_3(float *a, bool *c0, bool *c1, bool *s, bool *p, int *p0, int *p1, int n, int m)
+{
+  while (!munkres_step_3_all_covered(a, c0, c1, n, m)) 
+  {
+    if (munkres_step_3_prime(a, c0, c1, s, p, p0, p1, n, m)) {
+      return true;
+    }
+  }
+  return false;
 }
--- test/test_munkres.cpp
+++ test/test_munkres.cpp
@@ -69,7 +69,7 @@ TEST(munkres_step_1, Correct)
   assert_all_equal(s_true, s, n * m);
 }
 
-TEST(munkres_step_1, ShouldReturnTrue)
+TEST(munkres_step_2, ShouldReturnTrue)
 {
   const int n = 3;
   const int m = 4;
@@ -87,7 +87,7 @@ TEST(munkres_step_1, ShouldReturnTrue)
   ASSERT_EQ(true, done);
 }
 
-TEST(munkres_step_1, ShouldReturnFalse)
+TEST(munkres_step_2, ShouldReturnFalse)
 {
   const int n = 3;
   const int m = 4;
@@ -103,6 +103,155 @@ TEST(munkres_step_1, ShouldReturnFalse)
 
   bool done = munkres_step_2(s, c1, n, m);
   ASSERT_EQ(false, done);
+}
+
+TEST(munkres_step_3_all_covered, Correct)
+{
+  const int n = 3;
+  const int m = 4;
+
+  float a[n * m] = {
+    0, 1, 3, 2,
+    1, 0, 2, 0,
+    1, 3, 0, 1
+  };
+
+  {
+  bool c0[n] = { 0, 0, 0 };
+  bool c1[m] = { 0, 0, 0, 0 };
+  ASSERT_EQ(false, munkres_step_3_all_covered(a, c0, c1, n, m));
+  }
+
+  {
+  bool c0[n] = { 1, 1, 1 };
+  bool c1[m] = { 0, 0, 0, 0 };
+  ASSERT_EQ(true, munkres_step_3_all_covered(a, c0, c1, n, m));
+  }
+
+  {
+  bool c0[n] = { 0, 0, 0 };
+  bool c1[m] = { 1, 1, 1, 1 };
+  ASSERT_EQ(true, munkres_step_3_all_covered(a, c0, c1, n, m));
+  }
+
+  {
+  bool c0[n] = { 0, 0, 0 };
+  bool c1[m] = { 1, 0, 1, 1 };
+  ASSERT_EQ(false, munkres_step_3_all_covered(a, c0, c1, n, m));
+  }
+
+  {
+  bool c0[n] = { 1, 0, 1 };
+  bool c1[m] = { 0, 1, 0, 1 };
+  ASSERT_EQ(true, munkres_step_3_all_covered(a, c0, c1, n, m));
+  }
+}
+
+TEST(munkres_step_3_prime, Correct)
+{
+  const int n = 3;
+  const int m = 4;
+
+  float a[n * m] = {
+    0, 1, 0, 2,
+    1, 0, 2, 3,
+    1, 0, 1, 1
+  };
+
+  bool s[n * m] = {
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+
+  bool c0[n] = {
+    0, 0, 0 
+  };
+  bool c1[m] = { 
+    1, 1, 0, 0
+  };
+
+  bool p[n * m] = {
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+  };
+  int p0, p1;
+  bool step4 = munkres_step_3_prime(a, c0, c1, s, p, &p0, &p1, n, m);
+  ASSERT_EQ(false, step4);
+}
+
+TEST(munkres_step_3_prime, Correct2)
+{
+  const int n = 3;
+  const int m = 4;
+
+  float a[n * m] = {
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+    0, 1, 0, 0
+  };
+
+  bool s[n * m] = {
+    1, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+
+  bool c0[n] = {
+    1, 0, 0 
+  };
+  bool c1[m] = { 
+    0, 0, 0, 0
+  };
+
+  bool p[n * m] = {
+    0, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+  };
+
+  int p0, p1;
+  bool step4 = munkres_step_3_prime(a, c0, c1, s, p, &p0, &p1, n, m);
+  ASSERT_EQ(true, step4);
+  ASSERT_EQ(true, p[IDX_2D(1, 0, m)]);
+}
+
+TEST(munkres_step_3, Correct)
+{
+  const int n = 3;
+  const int m = 4;
+
+  float a[n * m] = {
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+    0, 1, 0, 0
+  };
+
+  bool s[n * m] = {
+    1, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+
+  bool c0[n] = {
+    0, 0, 0 
+  };
+  bool c1[m] = { 
+    1, 0, 0, 0
+  };
+
+  bool p[n * m] = {
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+  };
+
+  int p0, p1;
+  bool step4 = munkres_step_3(a, c0, c1, s, p, &p0, &p1, n, m);
+  ASSERT_EQ(true, step4);
+  ASSERT_EQ(true, p[IDX_2D(1, 0, m)]);
+  ASSERT_EQ(true, p[IDX_2D(0, 1, m)]);
 }
 
 #ifndef EXCLUDE_MAIN
