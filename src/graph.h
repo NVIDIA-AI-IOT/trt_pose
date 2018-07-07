50c4892a77f887f32e4da6fcc4f85208b0513c09
John Welsh <jwelsh@nvidia.com>
2018-07-07 19:29:21 +0000

    extern

--- src/graph.h
+++ src/graph.h
@@ -3,15 +3,23 @@
 
 #include "tensor.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct graph {
   int *data;
   int rows;
   int cols;
 } graph_t;
 
-inline int graph_at(graph_t *self, int row, int col)
+extern inline int graph_at(graph_t *self, int row, int col)
 {
   return self->data[IDX_2D(row, col, self->cols)];
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- src/weighted_graph.h
+++ src/weighted_graph.h
@@ -3,15 +3,23 @@
 
 #include "tensor.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct weighted_graph {
   float *data;
   int rows;
   int cols;
 } weighted_graph_t;
 
-inline float weighted_graph_at(weighted_graph_t *self, int row, int col)
+extern inline float weighted_graph_at(weighted_graph_t *self, int row, int col)
 {
   return self->data[IDX_2D(row, col, self->cols)];
 }
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
