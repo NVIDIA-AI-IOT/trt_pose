eec93b7c0f096513a3ed1644b6ec6f36f8814ee9
John Welsh <jwelsh@nvidia.com>
2019-07-29 15:47:53 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trt_pose',
+    version='0.0.0',
+    description='Pose detection accelerated by NVIDIA TensorRT',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
