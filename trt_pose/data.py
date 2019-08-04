a0c000314680d5cd1f901bd4d0a35323c48c8f68
John Welsh <jwelsh@nvidia.com>
2019-08-04 18:45:08 -0400

    added paf/cmap dataset generation

---
+++ trt_pose/data.py
@@ -0,0 +1,17 @@
+import torch
+
+
+def _coco_category_topology_tensor(coco_category):
+    skeleton = coco_category['skeleton']
+    K = len(skeleton)
+    topology = torch.zeros((K, 4)).int()
+    for k in range(K):
+        topology[k][0] = 2 * k
+        topology[k][1] = 2 * k + 1
+        topology[k][2] = skeleton[k][0] - 1
+        topology[k][3] = skeleton[k][1] - 1
+    return topology
+
+
+def _coco_category_parts(coco_category):
+    return coco_category['keypoints']
