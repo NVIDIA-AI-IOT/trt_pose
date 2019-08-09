212c5441073f11296e582b24dd668028fbdc66ec
John Welsh <jwelsh@nvidia.com>
2019-08-09 15:32:09 -0400

    removed examples

--- examples/coco_person/download_coco.sh
+++
@@ -1,3 +0,0 @@
-wget http://images.cocodataset.org/zips/train2017.zip
-wget http://images.cocodataset.org/zips/val2017.zip
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
