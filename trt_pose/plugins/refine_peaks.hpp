daceb807b236d6a4a4625e262eecde65e0519122
John Welsh <jwelsh@nvidia.com>
2020-02-29 14:26:04 -0800

    trt_pose cpp

--- trt_pose/plugins/refine_peaks.hpp
+++ trt_pose/plugins/refine_peaks.hpp
@@ -1,6 +1,38 @@
-#include <torch/extension.h>
-#include <vector>
+#pragma once
+
+void refine_peaks_out_hw(
+    float *refined_peaks, 
+    const int *counts,
+    const int *peaks,
+    const float *cmap,
+    const int H,
+    const int W,
+    const int M,
+    const int window_size
+);
+
+void refine_peaks_out_chw(
+    float *refined_peaks, 
+    const int *counts,
+    const int *peaks,
+    const float *cmap,
+    const int C,
+    const int H,
+    const int W,
+    const int M,
+    const int window_size
+);
 
 
-void refine_peaks_out(torch::Tensor refined_peaks, torch::Tensor counts, torch::Tensor peaks, torch::Tensor cmap, int window_size);
-torch::Tensor refine_peaks(torch::Tensor counts, torch::Tensor peaks, torch::Tensor cmap, int window_size);
+void refine_peaks_out_nchw(
+    float *refined_peaks, 
+    const int *counts,
+    const int *peaks,
+    const float *cmap,
+    const int N,
+    const int C,
+    const int H,
+    const int W,
+    const int M,
+    const int window_size
+);
--- trt_pose/plugins/test_all.cpp
+++ trt_pose/plugins/test_all.cpp
@@ -1,36 +1,31 @@
-#include "find_peaks.hpp"
 #include <stdexcept>
+#include "find_peaks.hpp"
+#include "refine_peaks.hpp"
 
+#define ABS(x) ((x) > 0 ? (x) : (-x))
 
 void test_find_peaks_out_hw()
 {
-  const int N = 1;
-  const int C = 2;
   const int H = 4;
   const int W = 4;
   const int M = 10;
   const float threshold = 2.0;
   const int window_size = 3;
 
-  int counts[N * C];
-  int peaks[N * C * M * 2];
-  float input[N * C * H * W] = {
+  int counts;
+  int peaks[M * 2];
+  const float input[H * W] = {
 
     0., 0., 0., 0.,
     0., 0., 3., 0.,
     0., 0., 0., 0.,
-    1., 0., 0., 0.,
-
-    0., 0., 0., 0.,
-    0., 0., 0., 0.,
-    0., 0., 0., 0.,
-    0., 0., 0., 0.
+    1., 0., 0., 0.
 
   };
 
-  find_peaks_out_nchw(counts, peaks, input, N, C, H, W, M, threshold, window_size);
+  find_peaks_out_hw(&counts, peaks, input, H, W, M, threshold, window_size);
 
-  if (counts[0] != 1) {
+  if (counts != 1) {
     throw std::runtime_error("Number of peaks should be 1.");
   }
   if (peaks[0] != 1) {
@@ -41,8 +36,43 @@ void test_find_peaks_out_hw()
   }
 }
 
+void test_refined_peaks_out_hw()
+{
+  const int H = 4;
+  const int W = 4;
+  const int M = 1;
+  const int window_size = 3;
+
+  const int counts = 1;
+  const int peaks[M * 2] = { 1, 2 };
+  const float cmap[H * W] = {
+
+    0., 0., 1., 0.,
+    0., 2., 3., 1.,
+    0., 0., 2., 0.,
+    0., 0., 0., 0.
+
+  };
+  const float i_true = (0.5 + (1. * 0 + 2. * 1 + 3. * 1 + 1. * 1 + 2. * 2) / 9.) / H;
+  const float j_true = (0.5 + (2. * 1 + 1. * 2 + 3. * 2 + 2. * 2 + 1. * 3) / 9.) / W;
+  const float tolerance = 1e-5;
+
+  float refined_peaks[M * 2];
+
+  refine_peaks_out_hw(refined_peaks, &counts, peaks, cmap, H, W, M, window_size);
+
+  if (ABS(refined_peaks[0] - i_true) > tolerance) {
+    throw std::runtime_error("i coordinate incorrect");
+  }
+  if (ABS(refined_peaks[1] - j_true) > tolerance) {
+    throw std::runtime_error("j coordinate incorrect");
+  }
+
+}
+
 int main()
 {
   test_find_peaks_out_hw();
+  test_refined_peaks_out_hw();
   return 0;
 }
