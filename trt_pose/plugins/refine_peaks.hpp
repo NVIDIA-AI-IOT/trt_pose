1a7ea4fa342dbe571c066106f9d13adeb7751920
John Welsh <jwelsh@nvidia.com>
2020-02-29 14:51:35 -0800

    reformatted refine peaks header

--- trt_pose/plugins/refine_peaks.hpp
+++ trt_pose/plugins/refine_peaks.hpp
@@ -1,38 +1,21 @@
 #pragma once
 
-void refine_peaks_out_hw(
-    float *refined_peaks, 
-    const int *counts,
-    const int *peaks,
-    const float *cmap,
-    const int H,
-    const int W,
-    const int M,
-    const int window_size
-);
+void refine_peaks_out_hw(float *refined_peaks, // Mx2
+                         const int *counts,    // 1
+                         const int *peaks,     // Mx2
+                         const float *cmap,    // HxW
+                         const int H, const int W, const int M,
+                         const int window_size);
 
-void refine_peaks_out_chw(
-    float *refined_peaks, 
-    const int *counts,
-    const int *peaks,
-    const float *cmap,
-    const int C,
-    const int H,
-    const int W,
-    const int M,
-    const int window_size
-);
+void refine_peaks_out_chw(float *refined_peaks, // CxMx2
+                          const int *counts,    // C
+                          const int *peaks,     // CxMx2
+                          const float *cmap, const int C, const int H,
+                          const int W, const int M, const int window_size);
 
-
-void refine_peaks_out_nchw(
-    float *refined_peaks, 
-    const int *counts,
-    const int *peaks,
-    const float *cmap,
-    const int N,
-    const int C,
-    const int H,
-    const int W,
-    const int M,
-    const int window_size
-);
+void refine_peaks_out_nchw(float *refined_peaks, // NxCxMx2
+                           const int *counts,    // NxC
+                           const int *peaks,     // NxCxMx2
+                           const float *cmap, const int N, const int C,
+                           const int H, const int W, const int M,
+                           const int window_size);
